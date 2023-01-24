c41465cb6c466a5e2ffc0e53bd17a91381ae49af
CodeX1991 <oghenewonaholy@gmail.com>
2023-01-24 11:03:50 -0800

    Fixiing _printf.c file

--- _printf.c
+++ _printf.c
@@ -11,15 +11,11 @@
 
 int _printf(const char *format, ...)
 {
-	int i, j;
-	int char_count = 0;
+	int i, char_count = 0;
 	va_list args;
 	char *str = NULL;
 
 	va_start(args, format);
-	
-	if (format == NULL)
-		return (-1);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
@@ -29,38 +25,24 @@ int _printf(const char *format, ...)
 			{
 				_putchar(va_arg(args, int));
 				char_count++;
-<<<<<<< HEAD
 				i += 2;
-=======
-				i++;
->>>>>>> 9507c78cecfa1c73a0832971c19822eaf7a8944e
 			}
 			else if (format[i + 1] == 's')
 			{
 				str = va_arg(args, char *);
-
-				for (j = 0; str[j] != '\0'; j++)
-				{
-					_putchar(str[j]);
-					char_count++;
-				}
-				i++;
+				char_count += print_str(str);
+				i += 2;
 			}
 			else if (format[i + 1] == '%')
 			{
 				_putchar('%');
 				char_count++;
-<<<<<<< HEAD
 				i += 2;
-=======
-				i++;
->>>>>>> 9507c78cecfa1c73a0832971c19822eaf7a8944e
 			}
 		}
 		_putchar(format[i]);
 		char_count++;
 	}
 	va_end(args);
-
 	return (char_count);
 }
--- main.h
+++ main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_str(char *s);
 
 #endif
